155e9ca9caaba3a81fa0b92f9ca155190afa685e
Bartek Florczak <bflorczak@outlook.com>
2023-09-05 12:36:18 +0200

    Initial commit

---
+++ runGridStandalone.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+docker stop selenium-standalone
+docker rm selenium-standalone
+
+IMAGE_VERSION='4.12'
+cat <<EOF >config.toml
+[docker]
+configs = [
+    "selenium/standalone-firefox:${IMAGE_VERSION}", '{"browserName": "firefox"}',
+    "selenium/standalone-chrome:${IMAGE_VERSION}", '{"browserName": "chrome"}'
+]
+url = "http://127.0.0.1:2375"
+[node]
+drivers = ["chrome", "firefox"]
+max-sessions = 4
+session-timeout = 3600
+grid-url = "http://localhost:4444"
+selenium-manager = true
+[sessionqueue]
+session-request-timeout = 1800
+EOF
+
+docker network create grid
+docker run -d -p 4444:4444 --shm-size="2g" \
+  --net grid \
+  --name selenium-standalone \
+  --health-cmd='/opt/bin/check-grid.sh --host 0.0.0.0 --port 4444' \
+  --health-interval=15s \
+  --health-timeout=30s \
+  --health-retries=5 \
+  -e SE_SCREEN_WIDTH=1920 \
+  -e SE_SCREEN_HEIGHT=1080 \
+  -e SE_VNC_NO_PASSWORD=1 \
+  -e SE_START_VNC=true \
+  -v "${PWD}"/config.toml:/opt/bin/config.toml \
+  -v "${PWD}"/assets:/opt/selenium/assets \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  selenium/standalone-docker:${IMAGE_VERSION}
+
+#waiting for grid to be ready
+set -e
+url="http://localhost:4444/wd/hub/status"
+wait_interval=1
+max_wait_time=45
+end_time=$((SECONDS + max_wait_time))
+time_left=$max_wait_time
+
+while [ $SECONDS -lt $end_time ]; do
+    response=$(curl -sL "$url" | jq -r '.value.ready')
+    if [ -n "$response"  ]  && [ "$response" ]; then
+        echo "Selenium Grid is up - executing tests"
+        break
+    else
+        echo "Waiting for the Grid. Sleeping for $wait_interval second(s). $time_left seconds left until timeout."
+        sleep $wait_interval
+        time_left=$((time_left - wait_interval))
+    fi
+done
+
+if [ $SECONDS -ge $end_time ]; then
+    echo "Timeout: The Grid was not started within $max_wait_time seconds."
+    exit 1
+fi
---
+++ src/test/java/seleniumdebug/NetworkCaptureTest.java
@@ -0,0 +1,126 @@
+package seleniumdebug;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ConditionFactory;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.NetworkInterceptor;
+import org.openqa.selenium.devtools.v115.network.Network;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.http.HttpResponse;
+import org.openqa.selenium.remote.http.Route;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BooleanSupplier;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.openqa.selenium.remote.http.Contents.utf8String;
+import static seleniumdebug.NetworkCaptureTest.AwaitUtil.awaitAssertTrue;
+
+public class NetworkCaptureTest {
+
+    private final AtomicReference<String> responseAsString = new AtomicReference<>();
+
+    private NetworkInterceptor interceptor;
+
+    private static DevTools enableInterception(HasDevTools augmented) {
+        var devTools = augmented.getDevTools();
+        devTools.createSessionIfThereIsNotOne();
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        return devTools;
+    }
+
+    private static Route getModifiedResponseFilter(AtomicBoolean flag, JsonNode map) {
+        return Route.matching(req -> req.getUri().endsWith("pong/get")).to(() -> req -> {
+            flag.set(true);
+            return new HttpResponse().setStatus(200).addHeader("Content-Type", "application/json").setContent(utf8String(map.toString()));
+        });
+    }
+
+    @BeforeMethod
+    public void setUp() throws JsonProcessingException, MalformedURLException {
+        var browser = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
+        var augmented = new Augmenter().augment(browser);
+        var devTools = enableInterception((HasDevTools) augmented);
+        captureResponse(devTools);
+        browser.get("https://developer.mozilla.org/en-US/");
+        var map = extractAndModifyCapturedResponse();
+        var flag = new AtomicBoolean(false);
+        interceptor = new NetworkInterceptor(augmented, getModifiedResponseFilter(flag, map));
+        browser.get("https://developer.mozilla.org/en-US/");
+        awaitAssertTrue(flag::get);
+    }
+
+    @Test
+    public void whenValidationErrorIsThrownOnApprovalUpdateFromDetailsThenErrorMessageIsDisplayedTest() throws InterruptedException {
+        interceptor.close();
+        Thread.sleep(Duration.ofSeconds(10).toMillis());
+    }
+
+    @AfterMethod
+    public void closeNetworkInterceptor() {
+        interceptor.close();
+    }
+
+    private void captureResponse(DevTools devTools) {
+        devTools.addListener(Network.responseReceived(), receivedResponse -> {
+            var response = receivedResponse.getResponse();
+            if (response.getUrl().endsWith("pong/get")) {
+                var requestId = receivedResponse.getRequestId();
+                responseAsString.set(devTools.send(Network.getResponseBody(requestId)).getBody());
+            }
+        });
+    }
+
+    private JsonNode extractAndModifyCapturedResponse() throws JsonProcessingException {
+        var map = new JsonMapper().readTree(AwaitUtil.awaitNonNull(responseAsString::get));
+        var colors = map.at("/hpMain/colors");
+        ((ObjectNode) colors).put("backgroundColor", "red");
+        return map;
+    }
+
+    static class AwaitUtil {
+        private static final Logger logger = LoggerFactory.getLogger(AwaitUtil.class);
+
+        private AwaitUtil() {
+            //Util class
+        }
+
+        public static ConditionFactory await() {
+            return Awaitility.await()
+                    .ignoreExceptions()
+                    .atMost(Duration.ofSeconds(10))
+                    .pollInterval(500, MILLISECONDS)
+                    .alias(String.valueOf(Thread.currentThread().getId()))
+                    .conditionEvaluationListener(condition -> logger.info("Is satisfied?: {}; Remaining seconds: {}", condition.isSatisfied(), Duration.ofMillis(condition.getRemainingTimeInMS()).getSeconds()));
+        }
+
+        public static <T> T awaitNonNull(Callable<T> callable) {
+            return await().until(callable, Objects::nonNull);
+        }
+
+        public static void awaitAssertTrue(BooleanSupplier actualProvider) {
+            await().untilAsserted(() -> assertThat(actualProvider.getAsBoolean(), is(true)));
+        }
+    }
+}
