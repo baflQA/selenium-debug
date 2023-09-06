7c2aa06c3e55bdfb5cfd9bbfd8ed0111c5677d57
Bartek Florczak <bflorczak@outlook.com>
2023-09-06 22:32:56 +0200

    Update NetworkCapture2Test.java

--- src/test/java/seleniumdebug/NetworkCapture2Test.java
+++ src/test/java/seleniumdebug/NetworkCapture2Test.java
@@ -4,44 +4,35 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.SneakyThrows;
 import org.awaitility.Awaitility;
-import org.awaitility.core.ConditionFactory;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.NetworkInterceptor;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.http.Contents;
 import org.openqa.selenium.remote.http.Filter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Objects;
-import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BooleanSupplier;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static seleniumdebug.NetworkCaptureTest.AwaitUtil.awaitAssertTrue;
+import static seleniumdebug.NetworkCapture2Test.AwaitUtil.awaitAssertTrue;
 
 public class NetworkCapture2Test {
 
-    private NetworkInterceptor interceptor;
-
     @SneakyThrows
-    @BeforeMethod
-    public void setUp() {
+    @Test
+    public void test() {
         var browser = new RemoteWebDriver(new URL("http://localhost:4444"), new ChromeOptions());
 //        var browser = new ChromeDriver();
         var augmented = new Augmenter().augment(browser);
         var flag = new AtomicBoolean(false);
-        interceptor = new NetworkInterceptor(augmented, (Filter) next -> req -> {
+        var interceptor = new NetworkInterceptor(augmented, (Filter) next -> req -> {
             var res = next.execute(req);
             if (req.getUri().endsWith("pong/get")) {
                 try {
@@ -53,43 +44,28 @@ public class NetworkCapture2Test {
                     res.setHeader("Content-Type", "application/json");
                     res.setContent(Contents.utf8String(map.toString()));
                     flag.set(true);
-                } catch (Exception e) {
+                } catch (Exception ignored) {
                 }
             }
             return res;
         });
         browser.get("https://developer.mozilla.org/en-US/");
         awaitAssertTrue(flag::get);
-    }
-
-    @Test
-    public void test() throws InterruptedException {
         interceptor.close();
         Thread.sleep(Duration.ofSeconds(10).toMillis());
     }
 
-    @AfterMethod
-    public void closeNetworkInterceptor() {
-        interceptor.close();
-    }
-
     static class AwaitUtil {
-        private static final Logger logger = LoggerFactory.getLogger(AwaitUtil.class);
-
         private AwaitUtil() {
             //Util class
         }
 
-        public static ConditionFactory await() {
-            return Awaitility.await().ignoreExceptions().atMost(Duration.ofSeconds(10)).pollInterval(500, MILLISECONDS).alias(String.valueOf(Thread.currentThread().getId())).conditionEvaluationListener(condition -> logger.info("Is satisfied?: {}; Remaining seconds: {}", condition.isSatisfied(), Duration.ofMillis(condition.getRemainingTimeInMS()).getSeconds()));
-        }
-
-        public static <T> T awaitNonNull(Callable<T> callable) {
-            return await().until(callable, Objects::nonNull);
-        }
-
         public static void awaitAssertTrue(BooleanSupplier actualProvider) {
-            await().untilAsserted(() -> assertThat(actualProvider.getAsBoolean(), is(true)));
+            Awaitility.await()
+                    .ignoreExceptions()
+                    .atMost(Duration.ofSeconds(10))
+                    .pollInterval(500, MILLISECONDS)
+                    .untilAsserted(() -> assertThat(actualProvider.getAsBoolean(), is(true)));
         }
     }
 }
